41a0ae2d463ae40f996caf2ce5109eeeebe3a887
siddhantkhariwal <siddhantkhariwal2012@gmail.com>
2024-10-12 16:05:29 +0530

    updated readme

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const puppeteer = require("puppeteer");
 const fs = require('fs');
 const { Parser } = require('json2csv');
 
+const url = 'https://www.google.com/localservices/prolist?g2lbs=AOHF13ni8QCEjseO7c_iX6TWEAVMZDPKH3m8UoRJ0ScoN5OJL8im9C2f_gPdnZte2igUsmNqU_NAFZ6q5ewaGAiD9dYgP0ZKfk07BH4o4_hfNnuxoDdxWN0%3D&hl=en-IN&gl=in&cs=1&ssta=1&oq=laundry%20in%20gurugram&src=2&sa=X&q=laundry%20in%20gurugram&ved=2ahUKEwiN-p2GkauHAxXU0IQAHX-SBlAQjdcJegQIABAF&slp=MgBAAVIECAIgAIgBAJoBBgoCFxkQAA%3D%3D&scp=ChBnY2lkOmRyeV9jbGVhbmVyEgAaACoLRHJ5IGNsZWFuZXI%3D';
+const filename = 'laundries.csv';
+
 (async () => {
     try {
-        const browser = await puppeteer.launch({ headless: true});
+        const browser = await puppeteer.launch({ headless: false});
         const page = await browser.newPage();
         await page.setViewport({ width: 1920, height: 1080 });
 
-        await page.goto('https://www.google.com/localservices/prolist?g2lbs=AOHF13ni8QCEjseO7c_iX6TWEAVMZDPKH3m8UoRJ0ScoN5OJL8im9C2f_gPdnZte2igUsmNqU_NAFZ6q5ewaGAiD9dYgP0ZKfk07BH4o4_hfNnuxoDdxWN0%3D&hl=en-IN&gl=in&cs=1&ssta=1&oq=laundry%20in%20gurugram&src=2&sa=X&q=laundry%20in%20gurugram&ved=2ahUKEwiN-p2GkauHAxXU0IQAHX-SBlAQjdcJegQIABAF&slp=MgBAAVIECAIgAIgBAJoBBgoCFxkQAA%3D%3D&scp=ChBnY2lkOmRyeV9jbGVhbmVyEgAaACoLRHJ5IGNsZWFuZXI%3D');
+        await page.goto(url);
 
         const laundries = [];
 
@@ -60,7 +63,7 @@ const { Parser } = require('json2csv');
         const parser = new Parser();
         const csv = parser.parse(laundries);
 
-        fs.writeFileSync('laundries.csv', csv);
+        fs.writeFileSync(filename, csv);
 
         await browser.close();
 
